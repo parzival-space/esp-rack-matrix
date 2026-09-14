891a982fcd8cf06c881629f200eea1c68f0c085c
Malte Linke <me@parzival.space>
2026-09-14 02:28:19 +0200

    chore: replace `printf` with `ESP_LOG_LEVEL_LOCAL` for logging

--- src/display.cpp
+++ src/display.cpp
@@ -7,9 +7,7 @@
 void display_task(void *arguments) {
     // init display
     U8X8_ESP32_MAX7219_8X8_4W_SW_SPI u8x8(
-        3,
-        2,
-        1,
+        3,2,1,
         U8X8_ESP32_PIN_NONE
     );
     u8x8.begin();
--- src/u8x8_esp32.cpp
+++ src/u8x8_esp32.cpp
@@ -7,6 +7,7 @@ extern "C" {
     #include <freertos/task.h>
     #include <driver/gpio.h>
     #include <rom/ets_sys.h>
+    #include <esp_log.h>
 }
 
 uint8_t u8x8_esp32_gpio_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
@@ -21,7 +22,7 @@ uint8_t u8x8_esp32_gpio_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int,
                     if (pin == U8X8_ESP32_PIN_NONE) continue;
 
                     if (i < U8X8_PIN_OUTPUT_CNT) {
-                        printf("Setup %d as output\n", pin);
+                        ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, "u8x8_esp32", "Setup %d as output", pin);
                         gpio_config_t output{};
                         output.mode = GPIO_MODE_OUTPUT;
                         output.intr_type = GPIO_INTR_DISABLE;
@@ -30,7 +31,7 @@ uint8_t u8x8_esp32_gpio_and_delay_cb(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int,
                         output.pull_up_en = GPIO_PULLUP_DISABLE;
                         gpio_config(&output);
                     } else {
-                        printf("Setup %d as input\n", pin);
+                        ESP_LOG_LEVEL_LOCAL(ESP_LOG_INFO, "u8x8_esp32", "Setup %d as input", pin);
                         gpio_config_t input{};
                         input.mode = GPIO_MODE_INPUT;
                         input.intr_type = GPIO_INTR_DISABLE;
